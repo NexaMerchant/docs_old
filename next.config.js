56df408c36d196d0447e0e525e01c491143fa3da
Steve <34465153+xxl4@users.noreply.github.com>
2024-06-19 16:36:56 +0800

    fix

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
+    experimental: {
+        appDir: true,
+    }
   };
   
   module.exports = nextConfig;
