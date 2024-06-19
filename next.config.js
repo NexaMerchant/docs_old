df07710af594cfbb3e97ae19db892ff094255851
Steve <34465153+xxl4@users.noreply.github.com>
2024-06-19 18:33:55 +0800

    fix next config

--- next.config.js
+++ next.config.js
@@ -2,9 +2,14 @@
 const nextConfig = {
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
-    experimental: {
-        images: true,
-    },
+    images: {
+        domains: ['github.io'],
+      },
+      experimental: {
+        // Other experimental features can go here
+        modern: true,
+        scrollRestoration: true,
+      },
   };
   
   module.exports = nextConfig;
